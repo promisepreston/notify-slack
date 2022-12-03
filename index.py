432cb3d7cc005aa91fd0607284c0cbf53fc2fada
Promise Chukwuenyem <promisepreston@gmail.com>
2022-12-03 15:22:21 +0100

    Update index.py and config.yml

--- index.py
+++ index.py
@@ -12,21 +12,32 @@ USERNAME = os.environ.get("SLACK_USERNAME")
 ICON_EMOJI = os.environ.get("SLACK_ICON_EMOJI")
 
 # Define lambda function
-def lambda_handler(event, context):
+def handler(event, context):
     url = WEBHOOK_URL
+    payload = extractMessage(event["Records"][0]["Sns"]["Message"])
     msg = {
-        "channel": CHANNEL,
-        "username": USERNAME,
-        "text": event["Records"][0]["Sns"]["Message"],
-        "icon_emoji": ICON_EMOJI,
+			"channel": CHANNEL,
+			"username": USERNAME,
+			"text": payload,
+			"icon_emoji": ICON_EMOJI,
     }
 
     encoded_msg = json.dumps(msg).encode("utf-8")
+    print(encoded_msg)
     resp = http.request("POST", url, body=encoded_msg)
     print(
-        {
-            "message": event["Records"][0]["Sns"]["Message"],
-            "status_code": resp.status,
-            "response": resp.data,
-        }
+			{
+				"message": payload,
+				"status_code": resp.status,
+				"response": resp.data,
+			}
     )
+
+# Format json payload
+def extractMessage(msg):
+	try:
+		alarmDic = json.loads(msg)
+	except:
+		print("Error while converting payload to json")
+		return
+	return "AlarmName: %s \AlarmDescription: %s" % (alarmDic["AlarmName"], alarmDic["AlarmDescription"])
